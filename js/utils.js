693e9f18fe0058842e51bb95650120cd99737f40
nilsmanc <etereaiberia@gmail.com>
2023-08-11 07:23:28 +0300

    shuffle, position, message

---
+++ js/utils.js
@@ -0,0 +1,45 @@
+export const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]
+  }
+
+  return array
+}
+
+export const isValidPos = (tetromino, tetrominoRow, tetrominoCol, playArea) => {
+  for (let row = 0; row < tetromino.length; row++) {
+    for (let col = 0; col < tetromino[row].length; col++) {
+      if (
+        tetromino[row][col] &&
+        (tetrominoCol + col < 0 ||
+          tetrominoCol + col >= playArea[0].length ||
+          tetrominoRow + row >= playArea.length ||
+          playArea[tetrominoRow + row][tetrominoCol + col])
+      ) {
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
+export const showGameMessage = (context, canvas, text) => {
+  context.fillStyle = 'black'
+  context.globalAlpha = 0.75
+  context.fillRect(0, canvas.height / 2 - 30, canvas.width, 60)
+  context.globalAlpha = 1
+  context.fillStyle = 'white'
+  context.font = '36px monospace'
+  context.textAlign = 'center'
+  context.textBaseline = 'middle'
+  context.fillText(text, canvas.width / 2, canvas.height / 2)
+}
