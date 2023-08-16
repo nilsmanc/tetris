8a47b46cc846b5722912cc4a68c19d94d7c9f381
nilsmanc <etereaiberia@gmail.com>
2023-08-16 08:22:48 +0300

    next tetromino

--- js/utils.js
+++ js/utils.js
@@ -86,3 +86,31 @@ export const moveOnClickLeft = (tetromino, playArea) => {
     tetromino.col = col
   }
 }
+
+export const showNextTetromino = (name) => {
+  const block = document.querySelector('.tetromino')
+
+  switch (name) {
+    case 'I':
+      block.className = 'tetromino tetromino-i'
+      break
+    case 'Z':
+      block.className = 'tetromino tetromino-z'
+      break
+    case 'S':
+      block.className = 'tetromino tetromino-s'
+      break
+    case 'T':
+      block.className = 'tetromino tetromino-t'
+      break
+    case 'O':
+      block.className = 'tetromino tetromino-o'
+      break
+    case 'L':
+      block.className = 'tetromino tetromino-l'
+      break
+    case 'J':
+      block.className = 'tetromino tetromino-j'
+      break
+  }
+}
